606f97b077cb10c3625a3e75dc5dfb86a2f56f8d
Krzysztof Kasprzak <timmmc88@gmail.com>
2020-11-20 18:37:22 +0000

    the app saves the location in coockies as well

--- app.rb
+++ app.rb
@@ -22,5 +22,16 @@ get '/temp' do
   json text: session[:text]
 end
 
+get '/location' do
+  p json loc: session[:loc]
+end
+
+post '/location' do
+  params = JSON.parse(request.body.read)
+  loc = params["loc"]
+  session[:loc] = loc
+  json loc: session[:loc]
+end
+
   run! if app_file == $0
 end
